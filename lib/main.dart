1186f73611ab2904ac948f1ee34db1c791bc9269
jaemin720 <hjm20720@naver.com>
2024-05-26 04:24:11 +0900

    jido

--- lib/main.dart
+++ lib/main.dart
@@ -1,125 +1,188 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(const MyApp());
-}
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Daejeon Map'),
+        ),
+        body: Center(
+          child: Stack(
+            children: [
+              ClipPath(
+                clipper: YuseongClipper(),
+                child: Container(
+                  width: 300,
+                  height: 400,
+                  decoration: BoxDecoration(
+                    image: DecorationImage(
+                      image: NetworkImage('https://www.wikihow.com/images_en/thumb/2/2f/769114-9.jpg/v4-460px-769114-9.jpg'),
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                ),
+              ),
+              ClipPath(
+                clipper: DaedeokClipper(),
+                child: Container(
+                  width: 300,
+                  height: 400,
+                  decoration: BoxDecoration(
+                    image: DecorationImage(
+                      image: NetworkImage('https://velog.velcdn.com/images/io_/post/fe69f46a-5331-4e65-84f0-84d69a9d25ec/image.png'),
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                ),
+              ),
+              ClipPath(
+                clipper: SeoClipper(),
+                child: Container(
+                  width: 300,
+                  height: 400,
+                  decoration: BoxDecoration(
+                    image: DecorationImage(
+                      image: NetworkImage('https://blog-cdn.tosspayments.com/wp-content/uploads/2022/04/07171631/copyright-5-thumb.jpg'),
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                ),
+              ),
+              ClipPath(
+                clipper: JungClipper(),
+                child: Container(
+                  width: 300,
+                  height: 400,
+                  decoration: BoxDecoration(
+                    image: DecorationImage(
+                      image: NetworkImage('https://www.wikihow.com/images_en/thumb/c/cc/769114-7.jpg/v4-460px-769114-7.jpg'),
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                ),
+              ),
+              ClipPath(
+                clipper: DongClipper(),
+                child: Container(
+                  width: 300,
+                  height: 400,
+                  decoration: BoxDecoration(
+                    image: DecorationImage(
+                      image: NetworkImage('https://blog-cdn.tosspayments.com/wp-content/uploads/2022/04/07171631/copyright-5-thumb.jpg'),
+                      fit: BoxFit.cover,
+                    ),
+                  ),
+                ),
+              ),
+            ],
+          ),
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
+  }
+}
+
+class YuseongClipper extends CustomClipper<Path> {
+  @override
+  Path getClip(Size size) {
+    Path path = Path();
+    path.moveTo(size.width * 0.02, size.height * 0.02);
+    path.lineTo(size.width * 0.45, size.height * 0.02);
+    path.quadraticBezierTo(size.width * 0.50, size.height * 0.10, size.width * 0.50, size.height * 0.20);
+    path.quadraticBezierTo(size.width * 0.50, size.height * 0.25, size.width * 0.45, size.height * 0.30);
+    path.quadraticBezierTo(size.width * 0.40, size.height * 0.35, size.width * 0.40, size.height * 0.40);
+    path.quadraticBezierTo(size.width * 0.40, size.height * 0.50, size.width * 0.45, size.height * 0.60);
+    path.lineTo(size.width * 0.02, size.height * 0.60);
+    path.close();
+    return path;
+  }
+
+  @override
+  bool shouldReclip(covariant CustomClipper<Path> oldClipper) {
+    return false;
+  }
+}
+
+
+class DaedeokClipper extends CustomClipper<Path> {
+  @override
+  Path getClip(Size size) {
+    Path path = Path();
+    path.moveTo(size.width * 0.45, size.height * 0.02);
+    path.lineTo(size.width * 0.98, size.height * 0.18);
+    path.lineTo(size.width * 0.88, size.height * 0.64);
+    path.lineTo(size.width * 0.52, size.height * 0.48);
+    path.lineTo(size.width * 0.45, size.height * 0.34);
+    path.close();
+    return path;
+  }
+
+  @override
+  bool shouldReclip(covariant CustomClipper<Path> oldClipper) {
+    return false;
+  }
+}
+
+class SeoClipper extends CustomClipper<Path> {
+  @override
+  Path getClip(Size size) {
+    Path path = Path();
+    path.moveTo(size.width * 0.02, size.height * 0.30);
+    path.lineTo(size.width * 0.45, size.height * 0.34);
+    path.lineTo(size.width * 0.53, size.height * 0.48);
+    path.lineTo(size.width * 0.45, size.height * 0.70);
+    path.lineTo(size.width * 0.02, size.height * 0.62);
+    path.close();
+    return path;
+  }
+
+  @override
+  bool shouldReclip(covariant CustomClipper<Path> oldClipper) {
+    return false;
+  }
+}
+
+class JungClipper extends CustomClipper<Path> {
+  @override
+  Path getClip(Size size) {
+    Path path = Path();
+    path.moveTo(size.width * 0.45, size.height * 0.34);
+    path.lineTo(size.width * 0.52, size.height * 0.48);
+    path.lineTo(size.width * 0.60, size.height * 0.48);
+    path.lineTo(size.width * 0.60, size.height * 0.62);
+    path.lineTo(size.width * 0.52, size.height * 0.76);
+    path.lineTo(size.width * 0.45, size.height * 0.76);
+    path.lineTo(size.width * 0.38, size.height * 0.62);
+    path.close();
+    return path;
+  }
+
+  @override
+  bool shouldReclip(covariant CustomClipper<Path> oldClipper) {
+    return false;
+  }
+}
+
+class DongClipper extends CustomClipper<Path> {
+  @override
+  Path getClip(Size size) {
+    Path path = Path();
+    path.moveTo(size.width * 0.50, size.height * 0.20);
+    path.lineTo(size.width * 0.56, size.height * 0.30);
+    path.lineTo(size.width * 0.70, size.height * 0.35);
+    path.lineTo(size.width * 0.65, size.height * 0.45);
+    path.lineTo(size.width * 0.55, size.height * 0.50);
+    path.lineTo(size.width * 0.50, size.height * 0.48);
+    path.close();
+    return path;
+  }
+
+  @override
+  bool shouldReclip(covariant CustomClipper<Path> oldClipper) {
+    return false;
   }
 }
